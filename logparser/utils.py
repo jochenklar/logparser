ef785ca9eb4c4f60802e0135b34a62574c52653d
Jochen Klar <mail@jochenklar.de>
2024-11-13 16:04:51 +0100

    Add get_first_line and get_last_line to utils

--- logparser/utils.py
+++ logparser/utils.py
@@ -2,6 +2,7 @@ import gzip
 import hashlib
 import logging
 import lzma
+import os
 import secrets
 import string
 import sys
@@ -10,11 +11,12 @@ from pathlib import Path
 logger = logging.getLogger(__name__)
 
 
-def open_log_file(log_path, mode='rt'):
+def open_log_file(log_path, mode='rt', log=True):
     if log_path is None:
         return sys.stdout
 
-    logger.info('%s %s (%s)', 'read' if mode.startswith('r') else 'open', log_path, mode)
+    if log:
+        logger.info('%s %s (%s)', 'read' if mode.startswith('r') else 'open', log_path, mode)
 
     log_path = Path(log_path)
 
@@ -50,3 +52,19 @@ def get_sha1(string):
 def get_random_salt():
     alphabet = string.ascii_letters + string.digits
     return ''.join(secrets.choice(alphabet) for i in range(8))
+
+
+def get_first_line(path):
+    with open_log_file(path, 'rb', log=False) as fp:
+        return fp.readline().decode()
+
+
+def get_last_line(path):
+    try:
+        with open_log_file(path, 'rb', log=False) as fp:
+            fp.seek(-2, os.SEEK_END)
+            while fp.read(1) != b'\n':
+                fp.seek(-2, os.SEEK_CUR)
+            return fp.readline().decode()
+    except (OSError, FileNotFoundError):
+        return None
